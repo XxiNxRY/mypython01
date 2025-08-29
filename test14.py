cd82af5a5024d8afd67b499e56ac27ab8b1dbf24
XxiNxRY <s6652410008@sau.ac.th>
2025-08-29 11:22:57 +0700

    Do control flow selection comlete

---
+++ test14.py
@@ -0,0 +1,17 @@
+# if-elif-else
+
+score = 68
+
+if score >= 80 :
+    print('Grade A')
+elif score >= 70 :
+    print('Grade B')
+elif score >= 60 :
+    print('Grade C')
+elif score >= 50 :
+    print('Grade D')
+else : #colon อย่าลืม *** else สุดท้ายจะเป็น elif ก็ได้
+    print('Grade F')
+
+print('Bye bey...')
+print('Ei ei...')
